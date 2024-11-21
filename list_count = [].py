e60660766adba314bf3fc09f0cd4c354059862be
kakshka988 <sobirid609@gmail.com>
2024-11-21 19:45:30 +0300

    create ex

---
+++ list_count = [].py
@@ -0,0 +1,15 @@
+list_count = []
+while True: 
+    S - input('Введите рост: ')
+    if s == '!' :
+        break
+    else:
+        if int(S) >= 160 and int (s)
+    <= 180:
+list_count.append(int(s))
+count +=1
+
+print("Количевство пройденных:" ,count)
+print("Максимальное значение: ")
+max(list_count)
+min(list_count)
